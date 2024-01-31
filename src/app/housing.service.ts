459b3cd14f6836c80e173a07edb0a49d47c9c913
Jwalts23 <bwbella0123@gmail.com>
2024-01-31 11:58:02 -0500

    Lesson 12 Complete

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -7,7 +7,7 @@ import { HousingLocation } from './housinglocation';
 export class HousingService {
 
   readonly baseUrl = 'https://angular.io/assets/images/tutorials/faa';
-  
+
   housingLocationList: HousingLocation[] = [
     {
       id: 0,
@@ -120,4 +120,9 @@ export class HousingService {
   getHousingLocationById(id: number): HousingLocation | undefined {
     return this.housingLocationList.find(housingLocation => housingLocation.id === id);
   }
+
+  submitApplication(firstName: string, lastName: string, email: string) {
+    console.log(`Homes application received: firstName: ${firstName}, lastName: ${lastName}, email: ${email}.`);
+  }
+
 }
